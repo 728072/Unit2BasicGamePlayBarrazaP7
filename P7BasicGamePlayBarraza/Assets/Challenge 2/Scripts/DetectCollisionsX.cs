121b5dbba19ebf17c2ee3388c741f1a42a623745
728072 <728072@student.dusd.net>
2026-03-16 15:02:11 -0700

    sdf

---
+++ P7BasicGamePlayBarraza/Assets/Challenge 2/Scripts/DetectCollisionsX.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisionsX : MonoBehaviour
+{
+    public GameObject dog;
+    private void OnTriggerEnter(Collider dog)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ P7BasicGamePlayBarraza/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 0.5f;
+    private float spawnTime = 3.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomBall", startDelay, spawnTime);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+    }
+
+}
